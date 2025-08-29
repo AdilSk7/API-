e1faf6e00fa7f9f8ded2e60f58f1cf4b97d922ca
Adil Shaik <153796089+AdilSk7@users.noreply.github.com>
2025-08-29 06:33:24 +0000

    final commit

--- test_api.py
+++ test_api.py
@@ -1,16 +1,12 @@
 import requests
 
-# Your hosted API endpoint
 url = "https://bfhl-api-z1tv.onrender.com/bfhl"
 
-# The JSON payload we want to send (just like in the examples)
 payload = {
     "data": ["a", "1", "334", "4", "R", "$"]
 }
 
-# Send a POST request with JSON data
 response = requests.post(url, json=payload)
 
-# Print results
 print("Status code:", response.status_code)
 print("Response JSON:", response.json())
